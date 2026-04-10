b1b9eddd66f36e8d1d50d8225f48d3bb8e450f09
silver <cyb3rsilv3r@gmail.com>
2026-04-10 15:38:54 -0400

    questions 3-6 done

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -15,6 +15,42 @@ public class MathApp {
         double lowestCost = Math.min(carPrice,truckPrice);
         System.out.println("The lowest price is: $" + lowestCost);
 
+        // Question 3
+        double radius = 7.25;
+        double area = Math.PI * (radius * radius);
+        System.out.println(" The area of the circle is: " + area);
+
+        // Question 4
+        double variable = 5.0;
+        double squareRoot = Math.sqrt(variable);
+        System.out.println(" The square root is: " + squareRoot);
+
+        // Question 5
+        double x1 = 5;
+        double y1 = 10;
+        double x2 = 85;
+        double y2 = 50;
+        double xDiff = x2 - x1;
+        double yDiff = y2 - y1;
+        double xDiffsq = xDiff * xDiff;
+        double yDiffsq = yDiff * yDiff;
+        double xyDiffsum = xDiffsq +yDiffsq;
+        double sumSq = Math.sqrt(xyDiffsum);
+        System.out.println("The distance between points is: "+ sumSq);
+
+        // Question 6
+        double q6Variable = -3.8;
+        double absValue = Math.abs(q6Variable);
+        System.out.println("The absolute value is: " + absValue);
+
+        // Question 7
+
+
+
+
+        // Question 8
+
+
 
 
 
